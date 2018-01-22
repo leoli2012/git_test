053d60c3a0643c0ad6687cfe1969c1f9c25464a4
liyong <liyong@miao.cn>
2018-01-22 16:36:49 +0800

    a simple 3 layer dnn model

---
+++ simple_dnn_3_layer.py
@@ -0,0 +1,17 @@
+#!codig=utf-8
+
+import traceback
+try:
+    import numpy as np
+    import pandas as pd
+except Exception as e:
+    print(traceback.format_exc())
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
